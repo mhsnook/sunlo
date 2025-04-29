1047e70be81ebb49b976ead4e5d35ce3470b227a
Em <me@msnook.xyz>
2025-04-29 22:34:11 +0530

    WIP stash

--- src/components/app-sidebar-layout.tsx
+++ src/components/app-sidebar-layout.tsx
@@ -1,8 +1,28 @@
 import type { ReactNode } from '@tanstack/react-router'
 import { SidebarInset } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
+import {
+	useState,
+	createContext,
+	UIEventHandler,
+	ReactEventHandler,
+	useMemo,
+} from 'react'
+import { usePrevious } from '@uidotdev/usehooks'
+
+type ScrollStateEnum = 'initial' | 'down' | 'up' | 'same' | undefined
+
+export const ScrollContext = createContext<ScrollStateEnum>(undefined)
 
 export function AppSidebarLayout({ children }: { children: ReactNode }) {
+	const [scrollTop, setScrollTop] = useState<{
+		new: number | null
+		prev: number | null
+		direction: ScrollStateEnum
+	}>({ new: null, prev: null, direction: 'initial' })
+
+	console.log(`Log rendering,`, scrollTop)
+
 	return (
 		<div className="flex h-screen w-full overflow-hidden">
 			<AppSidebar />
@@ -10,8 +30,28 @@ export function AppSidebarLayout({ children }: { children: ReactNode }) {
 				<div
 					id="app-sidebar-layout-outlet"
 					className="w-app @container overflow-y-auto pb-6"
+					onScroll={(event) =>
+						setScrollTop((prev) => {
+							const prevY = prev.new
+							const newY = event.target.scrollTop
+							if (prevY === newY) return
+							const direction =
+								prevY === null ? 'initial'
+								: prevY === newY ? 'same'
+								: prevY < newY ? 'down'
+								: prevY > newY ? 'up'
+								: undefined
+							return {
+								prev: prevY,
+								new: newY,
+								direction,
+							}
+						})
+					}
 				>
-					{children}
+					<ScrollContext.Provider value={scrollTop.direction}>
+						{children}
+					</ScrollContext.Provider>
 				</div>
 			</SidebarInset>
 		</div>
