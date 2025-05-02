fcec0701977a16f7989327ee13a6a7996f2fce7f
Em <me@msnook.xyz>
2025-05-03 00:58:50 +0530

    Show recommended phrases and turn on this section

--- src/components/phrase-card.tsx
+++ src/components/phrase-card.tsx
@@ -1,24 +1,24 @@
 import { cn } from '@/lib/utils'
-import { PhraseStub } from '@/types/main'
+import { PhraseFull } from '@/types/main'
 import { buttonVariants } from '@/components/ui/button-variants'
 import { Link } from '@tanstack/react-router'
 
 type PhraseCardProps = {
-	phrase: PhraseStub
+	phrase: PhraseFull
 }
 
 export const PhraseCard = ({ phrase }: PhraseCardProps) => (
 	<Link
 		className={cn(
 			buttonVariants({ variant: 'link' }),
-			`s-link rounded border p-3`
+			`s-link m-1 rounded border p-3`
 		)}
 		to="/learn/$lang/$id"
-		params={{ lang: phrase.lang, id: phrase.id }}
+		params={{ lang: phrase.lang!, id: phrase.id! }}
 	>
 		<span className="font-semibold">{phrase.text}</span>{' '}
 		<span className="text-muted-foreground text-sm">
-			{phrase.translation[0].text}
+			{phrase.translations[0].text}
 		</span>
 	</Link>
 )
