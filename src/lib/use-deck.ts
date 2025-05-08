d8ed5f93e8d21f663f6533b29dff0cbd1a3f391f
Em <me@msnook.xyz>
2025-05-08 15:06:02 +0530

    Use default query options for react-query

--- src/lib/use-deck.ts
+++ src/lib/use-deck.ts
@@ -82,9 +82,6 @@ export const deckQueryOptions = (lang: string, userId: uuid | null) =>
 		queryKey: ['user', lang, 'deck'],
 		queryFn: async ({ queryKey }) => fetchDeck(queryKey[1]),
 		enabled: !!userId && !!lang,
-		gcTime: 1_200_000,
-		staleTime: 120_000,
-		refetchOnWindowFocus: false,
 	})
 export const useDeck = (lang: string) => {
 	const { userId } = useAuth()
