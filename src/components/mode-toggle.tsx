053cf7642135d582479e5085e731724910f2b3a8
Em Snook <me@msnook.xyz>
2025-05-15 10:45:14 +0530

    Fix this padding/offset in collapsed sidebar

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -25,7 +25,7 @@ export function ModeToggle() {
 					<DropdownMenuTrigger asChild>
 						<SidebarMenuButton
 							size="lg"
-							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground group-data-[collapsible=icon]:p-2!"
 						>
 							<Sun className="h-[1.3rem] w-[1.3rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
 							<Moon className="absolute h-[1.3rem] w-[1.3rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
