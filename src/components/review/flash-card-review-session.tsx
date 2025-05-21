042b05a6a76387b98259277f6a6bb678151f4bbc
Em <me@msnook.xyz>
2025-05-21 14:19:33 +0530

    Combine all the map'd content into one component
    This also un-does the sort of wonky thing about having many many
    cards mounted and rendered but only one variable for whether to show
    the translations and buttons or not. It always seemed like it'd make
    more sense to have each card know whether it should be shown or not,
    and now that's the way it works. (But that logic hasn't quite been
    adjusted yet so this is WIP with a regression.)
    
    And tweaks the <Card> markup in the WhenComplete component so we have
    one full self-contained card in that file and don't even have to import
    Card into the larger flash-card-review-session, so it's a component
    that just handles navigation and control, and each review-single-card
    has its own state, mutation, etc.

--- src/components/review/flash-card-review-session.tsx
+++ src/components/review/flash-card-review-session.tsx
@@ -1,33 +1,19 @@
 import { useCallback, useState } from 'react'
-import toast from 'react-hot-toast'
-import { Play, ChevronLeft, ChevronRight } from 'lucide-react'
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import type { pids, ReviewRow, TranslationRow, uuid } from '@/types/main'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { cn } from '@/lib/utils'
+import type { pids } from '@/types/main'
+
 import {
-	postReview,
-	updateReview,
 	getFromLocalStorage,
-	setFromLocalStorage,
 	getIndexOfNextUnfinishedCard,
 } from '@/lib/use-reviewables'
-import { PostgrestError } from '@supabase/supabase-js'
-import PhraseExtraInfo from '../phrase-extra-info'
-import Flagged from '../flagged'
-import PermalinkButton from '../permalink-button'
-import SharePhraseButton from '../share-phrase-button'
+
 import { useLoaderData } from '@tanstack/react-router'
-import { WhenComplete } from './when-review-complete-screen'
+import { WhenComplete } from '@/components/review/when-review-complete-screen'
+import { ReviewSingleCard } from '@/components/review/review-single-card'
 
 interface ComponentProps {
 	dailyCacheKey: Array<string>
-}
-
-const playAudio = (text: string) => {
-	toast(`Playing audio for: ${text}`)
-	// In a real application, you would trigger audio playback here
 }
 
 export function FlashCardReviewSession({ dailyCacheKey }: ComponentProps) {
@@ -39,7 +25,7 @@ export function FlashCardReviewSession({ dailyCacheKey }: ComponentProps) {
 	const [currentCardIndex, setCurrentCardIndex] = useState(() =>
 		getIndexOfNextUnfinishedCard(dailyCacheKey, 0)
 	)
-	const [showTranslation, setShowTranslation] = useState(false)
+
 	const { cardsMap, phrasesMap } = useLoaderData({
 		from: '/_user/learn/$lang',
 		select: (data) => ({
@@ -60,9 +46,8 @@ export function FlashCardReviewSession({ dailyCacheKey }: ComponentProps) {
 				setCurrentCardIndex(() =>
 					getIndexOfNextUnfinishedCard(dailyCacheKey, 0)
 				)
-			setShowTranslation(false)
 		},
-		[currentCardIndex, setCurrentCardIndex, setShowTranslation, dailyCacheKey]
+		[currentCardIndex, setCurrentCardIndex, dailyCacheKey]
 	)
 
 	const isComplete = currentCardIndex === pids.length
@@ -83,15 +68,13 @@ export function FlashCardReviewSession({ dailyCacheKey }: ComponentProps) {
 						<ChevronLeft className="me-1 size-4" /> Back one card
 					</Button>
 				</div>
-				<Card className="mx-auto flex h-[80vh] w-full flex-col">
-					{!isComplete ? null : (
-						<WhenComplete
-							pids={pids}
-							dailyCacheKey={dailyCacheKey}
-							go={() => navigateCards('unfinished')}
-						/>
-					)}
-				</Card>
+				{!isComplete ? null : (
+					<WhenComplete
+						pids={pids}
+						dailyCacheKey={dailyCacheKey}
+						go={() => navigateCards('unfinished')}
+					/>
+				)}
 			</div>
 			<div
 				className={`${isComplete ? 'hidden' : 'flex'} flex-col items-center justify-center gap-2`}
@@ -122,204 +105,19 @@ export function FlashCardReviewSession({ dailyCacheKey }: ComponentProps) {
 					</Button>
 				</div>
 
-				{pids.map((pid, i) => {
-					const phrase = phrasesMap[pid]
-					const card = cardsMap[pid]
-					if (!card || card === null) {
-						console.log(`Error on this card render:`, pid, phrase, card)
-						throw new Error(
-							'Trying to review a card that does not exist. (Consider refreshing the page.)'
-						)
-					}
-					return (
-						<Card
-							key={i}
-							className={cn(
-								`mx-auto h-[80vh] w-full flex-col`,
-								i === currentCardIndex ? 'flex' : 'hidden'
-							)}
-						>
-							<CardHeader className="flex flex-row items-center justify-end gap-2">
-								<PermalinkButton
-									to={'/learn/$lang/$id'}
-									params={{ lang: phrase.lang!, id: phrase.id! }}
-								/>
-								<SharePhraseButton lang={phrase.lang!} pid={phrase.id!} />
-								<PhraseExtraInfo lang={phrase.lang!} pid={phrase.id!} />
-							</CardHeader>
-							<CardContent
-								className={`flex grow flex-col items-center justify-center px-[10%] pt-0`}
-							>
-								<div className="mb-4 flex items-center justify-center">
-									<div className="mr-2 text-2xl font-bold">{phrase.text}</div>
-									<Flagged name="text_to_speech">
-										<Button
-											size="icon"
-											variant="secondary"
-											onClick={() => playAudio(phrase.text!)}
-											aria-label="Play original phrase"
-										>
-											<Play className="size-4" />
-										</Button>
-									</Flagged>
-								</div>
-								<div>
-									{!showTranslation ? null : (
-										phrase.translations.map((trans: TranslationRow) => (
-											<div key={trans.id} className="mt-4 flex items-center">
-												<span className="mr-2 rounded-md bg-gray-200 px-2 py-1 text-xs text-gray-700">
-													{trans.lang}
-												</span>
-												<div className="me-2 text-xl">{trans.text}</div>
-												<Flagged name="text_to_speech">
-													<Button
-														size="icon-sm"
-														variant="secondary"
-														onClick={() => playAudio(trans.text)}
-														aria-label="Play translation"
-													>
-														<Play className="size-4" />
-													</Button>
-												</Flagged>
-											</div>
-										))
-									)}
-								</div>
-							</CardContent>
-							<UserCardReviewScoreButtonsRow
-								dailyCacheKey={dailyCacheKey}
-								// lang={lang}
-								pid={pid}
-								user_card_id={card.id!}
-								isButtonsShown={showTranslation}
-								showTheButtons={() => setShowTranslation(true)}
-								proceed={() => {
-									navigateCards('next')
-								}}
-							/>
-						</Card>
-					)
-				})}
+				{pids.map((pid, i) => (
+					<ReviewSingleCard
+						key={i}
+						isCurrentlyShown={i === currentCardIndex}
+						dailyCacheKey={dailyCacheKey}
+						phrase={phrasesMap[pid]}
+						card={cardsMap[pid]}
+						proceed={() => {
+							navigateCards('next')
+						}}
+					/>
+				))}
 			</div>
 		</div>
-	)
-}
-
-interface CardInnerProps {
-	dailyCacheKey: Array<string>
-	// lang: string
-	pid: uuid
-	user_card_id: uuid
-	isButtonsShown: boolean
-	showTheButtons: () => void
-	proceed: () => void
-}
-
-function UserCardReviewScoreButtonsRow({
-	dailyCacheKey,
-	user_card_id,
-	pid,
-	// lang,
-	isButtonsShown,
-	showTheButtons,
-	proceed,
-}: CardInnerProps) {
-	const prevData = getFromLocalStorage<ReviewRow>([...dailyCacheKey, pid])
-
-	const queryClient = useQueryClient()
-	const { mutate, isPending } = useMutation<
-		ReviewRow,
-		PostgrestError,
-		{ score: number }
-	>({
-		mutationKey: [...dailyCacheKey, pid],
-		mutationFn: async ({ score }: { score: number }) => {
-			if (prevData?.score === score) return prevData
-
-			return prevData?.id ?
-					await updateReview({
-						score,
-						review_id: prevData.id,
-					})
-				:	await postReview({
-						score,
-						user_card_id,
-						day_session: dailyCacheKey[3],
-					})
-		},
-		onSuccess: (data) => {
-			if (data.score === 1)
-				toast('okay', { icon: '🤔', position: 'bottom-center' })
-
-			if (data.score === 2)
-				toast('okay', { icon: '🤷', position: 'bottom-center' })
-			if (data.score === 3)
-				toast('got it', { icon: '👍️', position: 'bottom-center' })
-			if (data.score === 4) toast.success('nice', { position: 'bottom-center' })
-
-			setFromLocalStorage([...dailyCacheKey, pid], data)
-			void queryClient.invalidateQueries({
-				queryKey: [...dailyCacheKey, pid],
-			})
-			setTimeout(proceed, 1500)
-		},
-		onError: (error) => {
-			toast.error(`There was some kind of error idk: ${error.message}`)
-			console.log(`Error posting review:`, error)
-		},
-	})
-
-	return (
-		<CardFooter className="flex flex-col">
-			{!isButtonsShown && !prevData ?
-				<Button className="mb-3 w-full" onClick={showTheButtons}>
-					Show Translation
-				</Button>
-			:	<div className="mb-3 grid w-full grid-cols-4 gap-2">
-					<Button
-						variant="destructive"
-						onClick={() => mutate({ score: 1 })}
-						disabled={isPending}
-						className={
-							prevData?.score === 1 ? 'ring-primary ring-2 ring-offset-3' : ''
-						}
-					>
-						Again
-					</Button>
-					<Button
-						variant="secondary"
-						onClick={() => mutate({ score: 2 })}
-						disabled={isPending}
-						className={
-							prevData?.score === 2 ? 'ring-primary ring-2 ring-offset-3' : ''
-						}
-					>
-						Hard
-					</Button>
-					<Button
-						variant="default"
-						onClick={() => mutate({ score: 3 })}
-						disabled={isPending}
-						className={cn(
-							'bg-green-500 hover:bg-green-600',
-							prevData?.score === 3 ? 'ring-primary ring-2 ring-offset-3' : ''
-						)}
-					>
-						Good
-					</Button>
-					<Button
-						variant="default"
-						className={cn(
-							'bg-blue-500 hover:bg-blue-600',
-							prevData?.score === 4 ? 'ring-primary ring-2 ring-offset-3' : ''
-						)}
-						onClick={() => mutate({ score: 4 })}
-						disabled={isPending}
-					>
-						Easy
-					</Button>
-				</div>
-			}
-		</CardFooter>
 	)
 }
--- src/components/review/when-review-complete-screen.tsx
+++ src/components/review/when-review-complete-screen.tsx
@@ -1,5 +1,5 @@
 import { pids } from '@/types/main'
-import { CardContent } from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import SuccessCheckmark from '@/components/success-checkmark'
 import {
@@ -21,35 +21,42 @@ export function WhenComplete({
 	const skippedCount = countSkippedCards(pids, dailyCacheKey)
 	const againCount = countAgainCards(pids, dailyCacheKey)
 	const unfinishedCount = countUnfinishedCards(pids, dailyCacheKey)
-	return unfinishedCount ?
+	return (
+		<Card className="mx-auto flex h-[80vh] w-full flex-col">
 			<CardContent className="flex grow flex-col items-center justify-center gap-6 pt-0 pb-16">
-				<h2 className="text-2xl font-bold">Almost there!</h2>
-				<p className="text-center text-lg">
-					You're at the end of the queue, but there are still{' '}
-					{skippedCount ?
-						<>{skippedCount} cards you skipped over</>
-					:	null}
-					{skippedCount && againCount ?
-						<>, and </>
-					:	null}
-					{againCount ?
-						<>{againCount} cards you asked to see again</>
-					:	null}
-					. Let's go back and finish them &mdash; you can do it!
-				</p>
-				<Button
-					size="lg"
-					onClick={() => {
-						resetExtrasPids(dailyCacheKey)
-						go()
-					}}
-				>
-					Review unfinished cards ({unfinishedCount})
-				</Button>
+				{unfinishedCount ?
+					<>
+						<h2 className="text-2xl font-bold">Almost there!</h2>
+						<p className="text-center text-lg">
+							You're at the end of the queue, but there are still{' '}
+							{skippedCount ?
+								<>{skippedCount} cards you skipped over</>
+							:	null}
+							{skippedCount && againCount ?
+								<>, and </>
+							:	null}
+							{againCount ?
+								<>{againCount} cards you asked to see again</>
+							:	null}
+							. Let's go back and finish them &mdash; you can do it!
+						</p>
+						<Button
+							size="lg"
+							onClick={() => {
+								resetExtrasPids(dailyCacheKey)
+								go()
+							}}
+						>
+							Review unfinished cards ({unfinishedCount})
+						</Button>
+					</>
+				:	<>
+						<h2 className="text-2xl font-bold">Good work!</h2>
+						<p className="text-lg">You've completed your review for today.</p>
+						<SuccessCheckmark />
+					</>
+				}
 			</CardContent>
-		:	<CardContent className="flex grow flex-col items-center justify-center gap-6 pt-0 pb-16">
-				<h2 className="text-2xl font-bold">Good work!</h2>
-				<p className="text-lg">You've completed your review for today.</p>
-				<SuccessCheckmark />
-			</CardContent>
+		</Card>
+	)
 }
