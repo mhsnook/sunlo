61e8fc7de2d5e7629e67a1e27067bee2398df185
Em Snook <me@msnook.xyz>
2025-05-23 20:35:53 +0530

    Use more specific (typed) helpers for local storage ops

--- src/routes/_user/learn.$lang.review.go.tsx
+++ src/routes/_user/learn.$lang.review.go.tsx
@@ -1,10 +1,12 @@
 import { createFileRoute, Navigate } from '@tanstack/react-router'
-
 import { FlashCardReviewSession } from '@/components/review/flash-card-review-session'
-import { getFromLocalStorage } from '@/lib/use-reviewables'
-import { useMemo } from 'react'
-import { pids } from '@/types/main'
+import {
+	getExtrasFromLocalStorage,
+	getManifestFromLocalStorage,
+} from '@/lib/use-reviewables'
+import { useState } from 'react'
 import { todayString } from '@/lib/utils'
+import { DailyCacheKey } from '@/types/main'
 
 export const Route = createFileRoute('/_user/learn/$lang/review/go')({
 	component: ReviewPage,
@@ -18,15 +20,22 @@ export const Route = createFileRoute('/_user/learn/$lang/review/go')({
 function ReviewPage() {
 	const { lang } = Route.useParams()
 	// referential stability for the cache key
-	const [dailyCacheKey] = useState(() => [
+	const [dailyCacheKey] = useState<DailyCacheKey>(() => [
 		'user',
 		lang,
 		'review',
 		todayString(),
 	])
 
-	const reviewPids = getFromLocalStorage<pids>(dailyCacheKey)
-	if (!reviewPids || !reviewPids.length) return <Navigate to=".." />
+	const reviewPids = getManifestFromLocalStorage(dailyCacheKey)
+	if (!reviewPids || !reviewPids.length)
+		return <Navigate to="/learn/$lang/review" params={{ lang }} />
 
-	return <FlashCardReviewSession dailyCacheKey={dailyCacheKey} />
+	return (
+		<FlashCardReviewSession
+			dailyCacheKey={dailyCacheKey}
+			extras={true}
+			lang={lang}
+		/>
+	)
 }
