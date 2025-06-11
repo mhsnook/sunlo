06ba2b9deb2d127f4c35e1679273bf10ef641d10
Em <me@msnook.xyz>
2025-06-11 18:09:45 +0530

    Fix the next-valid-card navigation in the review store

--- src/lib/use-review-store.ts
+++ src/lib/use-review-store.ts
@@ -1,6 +1,5 @@
-import { useMemo } from 'react'
 import { createStore } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import { useReviewStore } from '@/components/review/review-context-provider'
 import type {
 	DailyCacheKey,
@@ -13,8 +12,9 @@ import type {
 type ReviewsInProgressMap = Record<uuid, ReviewRow | null>
 
 const DEFAULT_PROPS = {
-	dayString: '',
 	lang: '',
+	dayString: '',
+	dailyCacheKey: ['user', '', 'review', ''] as DailyCacheKey,
 	stage: 0 as ReviewStages,
 	currentCardIndex: -1,
 	manifest: [] as pids,
@@ -44,88 +44,95 @@ export type ReviewStore = ReturnType<typeof createReviewStore>
 
 export function createReviewStore(lang: string, dayString: string) {
 	return createStore<ReviewState>()(
-		persist<ReviewState>(
-			(set) => ({
-				...DEFAULT_PROPS,
-				lang,
-				dayString,
-				actions: {
-					skipReviewUnreviewed: () => set({ stage: 3 }),
-					skipReviewAgains: () => set({ stage: 5 }),
+		devtools(
+			persist(
+				(set) => ({
+					...DEFAULT_PROPS,
+					lang,
+					dayString,
+					actions: {
+						skipReviewUnreviewed: () => set({ stage: 3 }),
+						skipReviewAgains: () => set({ stage: 5 }),
 
-					gotoReviewUnreviewed: () =>
-						set((state) => ({
-							stage: 2,
-							currentCardIndex: getIndexOfNextUnreviewedCard(state),
-						})),
-					gotoReviewAgains: () =>
-						set((state) => ({
-							stage: 4,
-							currentCardIndex: getIndexOfNextAgainCard(state),
-						})),
+						gotoReviewUnreviewed: () =>
+							set((state) => ({
+								stage: 2,
+								currentCardIndex: getIndexOfNextUnreviewedCard(state),
+							})),
+						gotoReviewAgains: () =>
+							set((state) => ({
+								stage: 4,
+								currentCardIndex: getIndexOfNextAgainCard(state),
+							})),
 
-					gotoNextValid: () =>
-						set((state) => ({
-							currentCardIndex:
-								state.stage < 3 ?
-									getIndexOfNextUnreviewedCard(state)
-								:	getIndexOfNextAgainCard(state),
-						})),
-					gotoNext: () =>
-						set((state) => ({ currentCardIndex: state.currentCardIndex + 1 })),
-					gotoPrevious: () =>
-						set((state) => ({ currentCardIndex: state.currentCardIndex - 1 })),
+						gotoNextValid: () =>
+							set((state) => ({
+								currentCardIndex:
+									state.stage < 3 ?
+										getIndexOfNextUnreviewedCard(state)
+									:	getIndexOfNextAgainCard(state),
+							})),
+						gotoNext: () =>
+							set((state) => ({
+								currentCardIndex: state.currentCardIndex + 1,
+							})),
+						gotoPrevious: () =>
+							set((state) => ({
+								currentCardIndex: state.currentCardIndex - 1,
+							})),
 
-					init: (manifest: pids, lang: string, dayString: string) =>
-						set((state) => {
-							console.log(`The set fn inside the init fn`, manifest)
-							// if the lang doesn't match, we have an error
-							if (lang !== state.lang)
-								throw new Error(
-									'Mismatching language codes between route param and context provider (active state)'
-								)
-							// ensure we only init once
-							if (state.manifest.length) return state
-							return {
-								// now we'll just assume these can be wiped out
-								lang,
-								dayString,
-								manifest,
-								manifestLength: manifest.length,
-								currentCardIndex: 0,
-								stage: 1,
-								reviewsMap: manifest.reduce<ReviewsInProgressMap>(
-									(acc: ReviewsInProgressMap, pid) => {
-										acc[pid] = null
-										return acc
-									},
-									{}
-								),
-							}
-						}),
+						init: (manifest: pids, lang: string, dayString: string) =>
+							set((state) => {
+								console.log(`The set fn inside the init fn`, manifest)
+								// if the lang doesn't match, we have an error
+								if (lang !== state.lang)
+									throw new Error(
+										'Mismatching language codes between route param and context provider (active state)'
+									)
+								// ensure we only init once
+								if (state.manifest.length) return state
+								return {
+									// now we'll just assume these can be wiped out
+									lang,
+									dayString,
+									dailyCacheKey: ['user', lang, 'review', dayString],
+									manifest,
+									manifestLength: manifest.length,
+									currentCardIndex: 0,
+									stage: 1,
+									reviewsMap: manifest.reduce<ReviewsInProgressMap>(
+										(acc: ReviewsInProgressMap, pid) => {
+											acc[pid] = null
+											return acc
+										},
+										{}
+									),
+								}
+							}),
 
-					addReview: (review: ReviewRow) =>
-						set((state) => ({
-							reviewsMap: {
-								...state.reviewsMap,
-								[review.phrase_id]: review,
-							},
-						})),
-				},
-			}),
-			{
-				name: `review-store-${lang}`,
-				partialize: (state) => ({
-					// Only persist the state you want
-					lang: state.lang,
-					dayString: state.dayString,
-					stage: state.stage,
-					currentCardIndex: state.currentCardIndex,
-					manifest: state.manifest,
-					manifestLength: state.manifestLength,
-					reviewsMap: state.reviewsMap,
+						addReview: (review: ReviewRow) =>
+							set((state) => ({
+								reviewsMap: {
+									...state.reviewsMap,
+									[review.phrase_id!]: review,
+								},
+							})),
+					},
 				}),
-			}
+				{
+					name: `review-store-${lang}`,
+					partialize: (state): ReviewProps => ({
+						lang: state.lang,
+						dayString: state.dayString,
+						dailyCacheKey: state.dailyCacheKey,
+						stage: state.stage,
+						currentCardIndex: state.currentCardIndex,
+						manifest: state.manifest,
+						manifestLength: state.manifestLength,
+						reviewsMap: state.reviewsMap,
+					}),
+				}
+			)
 		)
 	)
 }
@@ -134,7 +141,11 @@ function getIndexOfNextUnreviewedCard(state: ReviewState) {
 	const index = state.manifest.findIndex((pid, i) => {
 		// if we're currently at card 3 of 40, don't even check cards 0-3
 		// but if we're at 40/40 we should check from the start
-		if (i !== state.manifestLength && i <= state.currentCardIndex) return false
+		if (
+			state.currentCardIndex !== state.manifestLength &&
+			i <= state.currentCardIndex
+		)
+			return false
 		// if the entry is undefined, it means we haven't reviewed this card yet
 		return !state.reviewsMap[pid]
 	})
@@ -146,12 +157,12 @@ function getIndexOfNextAgainCard(state: ReviewState) {
 	const index = state.manifest.findIndex((_, i) => {
 		// we want first to check state.currentCardIndex + 1
 		const indexChecking =
-			state.currentCardIndex === state.manifestLength ?
-				i
-			:	(i + state.currentCardIndex + 1) % state.manifestLength
+			(i + state.currentCardIndex + 1) % state.manifestLength
 		return state.reviewsMap[state.manifest[indexChecking]]?.score === 1
 	})
-	return index !== -1 ? index : state.manifestLength
+	return index !== -1 ?
+			(index + state.currentCardIndex + 1) % state.manifestLength
+		:	state.manifestLength
 }
 
 export const useReviewLang = (): string => {
@@ -163,9 +174,7 @@ export const useReviewDayString = (): string => {
 }
 
 export const useReviewCacheKey = (): DailyCacheKey => {
-	const lang = useReviewLang()
-	const dayString = useReviewDayString()
-	return useMemo(() => ['user', lang, 'review', dayString], [lang, dayString])
+	return useReviewStore((store) => store.dailyCacheKey)
 }
 
 export const useReviewStage = (): ReviewStages => {
