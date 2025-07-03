29fdf7ddb529faf5a8119df08b84edabdc5da2e2
Em <me@msnook.xyz>
2025-07-03 17:11:04 +0530

    Add migration for this user_client_event table

---
+++ supabase/migrations/20250703105000_create_table_user_client_event.sql
@@ -0,0 +1,67 @@
+create table "public"."user_client_event" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "uid" uuid default auth.uid(),
+    "message" text,
+    "context" jsonb,
+    "url" text
+);
+
+
+alter table "public"."user_client_event" enable row level security;
+
+CREATE UNIQUE INDEX user_client_event_pkey ON public.user_client_event USING btree (id);
+
+alter table "public"."user_client_event" add constraint "user_client_event_pkey" PRIMARY KEY using index "user_client_event_pkey";
+
+grant delete on table "public"."user_client_event" to "anon";
+
+grant insert on table "public"."user_client_event" to "anon";
+
+grant references on table "public"."user_client_event" to "anon";
+
+grant select on table "public"."user_client_event" to "anon";
+
+grant trigger on table "public"."user_client_event" to "anon";
+
+grant truncate on table "public"."user_client_event" to "anon";
+
+grant update on table "public"."user_client_event" to "anon";
+
+grant delete on table "public"."user_client_event" to "authenticated";
+
+grant insert on table "public"."user_client_event" to "authenticated";
+
+grant references on table "public"."user_client_event" to "authenticated";
+
+grant select on table "public"."user_client_event" to "authenticated";
+
+grant trigger on table "public"."user_client_event" to "authenticated";
+
+grant truncate on table "public"."user_client_event" to "authenticated";
+
+grant update on table "public"."user_client_event" to "authenticated";
+
+grant delete on table "public"."user_client_event" to "service_role";
+
+grant insert on table "public"."user_client_event" to "service_role";
+
+grant references on table "public"."user_client_event" to "service_role";
+
+grant select on table "public"."user_client_event" to "service_role";
+
+grant trigger on table "public"."user_client_event" to "service_role";
+
+grant truncate on table "public"."user_client_event" to "service_role";
+
+grant update on table "public"."user_client_event" to "service_role";
+
+create policy "Enable insert for any user"
+on "public"."user_client_event"
+as permissive
+for insert
+to authenticated, anon
+with check (true);
+
+
+
