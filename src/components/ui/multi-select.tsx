ad9a7235f2d4b1d70edc21d35bac484d59a2ff50
Em <me@msnook.xyz>
2025-07-06 17:05:34 +0530

    Add preliminary version of fancy multi select

---
+++ src/components/ui/multi-select.tsx
@@ -0,0 +1,164 @@
+// copied from https://github.com/mxkaske/mxkaske.dev/blob/main/components/craft/fancy-multi-select.tsx
+
+import * as React from 'react'
+import { X } from 'lucide-react'
+
+import { Badge } from '@/components/ui/badge'
+import {
+	Command,
+	CommandGroup,
+	CommandItem,
+	CommandList,
+} from '@/components/ui/command'
+import { Command as CommandPrimitive } from 'cmdk'
+
+type Option = Record<'value' | 'label', string>
+
+const options = [
+	{
+		value: 'next.js',
+		label: 'Next.js',
+	},
+	{
+		value: 'sveltekit',
+		label: 'SvelteKit',
+	},
+	{
+		value: 'nuxt.js',
+		label: 'Nuxt.js',
+	},
+	{
+		value: 'remix',
+		label: 'Remix',
+	},
+	{
+		value: 'astro',
+		label: 'Astro',
+	},
+	{
+		value: 'wordpress',
+		label: 'WordPress',
+	},
+	{
+		value: 'express.js',
+		label: 'Express.js',
+	},
+	{
+		value: 'nest.js',
+		label: 'Nest.js',
+	},
+] satisfies Option[]
+
+export function FancyMultiSelect({ always }: { always?: string }) {
+	const inputRef = React.useRef<HTMLInputElement>(null)
+	const [open, setOpen] = React.useState(false)
+	const [selected, setSelected] = React.useState<Option[]>([options[1]])
+	const [inputValue, setInputValue] = React.useState('')
+
+	const handleUnselect = React.useCallback((framework: Option) => {
+		setSelected((prev) => prev.filter((s) => s.value !== framework.value))
+	}, [])
+
+	const handleKeyDown = React.useCallback(
+		(e: React.KeyboardEvent<HTMLDivElement>) => {
+			const input = inputRef.current
+			if (input) {
+				if (e.key === 'Delete' || e.key === 'Backspace') {
+					if (input.value === '') {
+						setSelected((prev) => {
+							const newSelected = [...prev]
+							newSelected.pop()
+							return newSelected
+						})
+					}
+				}
+				// This is not a default behaviour of the <input /> field
+				if (e.key === 'Escape') {
+					input.blur()
+				}
+			}
+		},
+		[]
+	)
+
+	const selectables = options.filter(
+		(framework) => !selected.includes(framework)
+	)
+
+	console.log(selectables, selected, inputValue)
+
+	return (
+		<Command
+			onKeyDown={handleKeyDown}
+			className="overflow-visible bg-transparent"
+		>
+			<div className="group border-primary-foresoft/30 ring-offset-background focus-within:ring-ring rounded-md border px-3 py-2 text-sm focus-within:ring-2 focus-within:ring-offset-2">
+				<div className="flex flex-wrap gap-1">
+					{always ?
+						<Badge variant="default">{always}</Badge>
+					:	null}
+					{selected.map((framework) => {
+						return (
+							<Badge key={framework.value} variant="secondary">
+								{framework.label}
+								<button
+									className="ring-offset-background focus:ring-ring ml-1 rounded-full outline-none focus:ring-2 focus:ring-offset-2"
+									onKeyDown={(e) => {
+										if (e.key === 'Enter') {
+											handleUnselect(framework)
+										}
+									}}
+									onMouseDown={(e) => {
+										e.preventDefault()
+										e.stopPropagation()
+									}}
+									onClick={() => handleUnselect(framework)}
+								>
+									<X className="text-muted-foreground hover:text-foreground h-3 w-3" />
+								</button>
+							</Badge>
+						)
+					})}
+					{/* Avoid having the "Search" Icon */}
+					<CommandPrimitive.Input
+						ref={inputRef}
+						value={inputValue}
+						onValueChange={setInputValue}
+						onBlur={() => setOpen(false)}
+						onFocus={() => setOpen(true)}
+						placeholder="Select frameworks..."
+						className="placeholder:text-muted-foreground ml-2 flex-1 bg-transparent outline-none"
+					/>
+				</div>
+			</div>
+			<div className="relative mt-2">
+				<CommandList>
+					{open && selectables.length > 0 ?
+						<div className="bg-popover text-popover-foreground animate-in absolute top-0 z-10 w-full rounded-md border shadow-md outline-none">
+							<CommandGroup className="h-full overflow-auto">
+								{selectables.map((framework) => {
+									return (
+										<CommandItem
+											key={framework.value}
+											onMouseDown={(e) => {
+												e.preventDefault()
+												e.stopPropagation()
+											}}
+											onSelect={(value) => {
+												setInputValue('')
+												setSelected((prev) => [...prev, framework])
+											}}
+											className={'cursor-pointer'}
+										>
+											{framework.label}
+										</CommandItem>
+									)
+								})}
+							</CommandGroup>
+						</div>
+					:	null}
+				</CommandList>
+			</div>
+		</Command>
+	)
+}
