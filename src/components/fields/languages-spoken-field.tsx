9dd1ab68adf9692a052ce951ce3d196dda62804f
Em <me@msnook.xyz>
2025-07-06 20:06:21 +0530

    Add placeholder and 'splainer text

--- src/components/fields/languages-spoken-field.tsx
+++ src/components/fields/languages-spoken-field.tsx
@@ -15,17 +15,24 @@ export default function LanguagesSpokenField<T extends FieldValues>({
 
 	return (
 		<div className="flex flex-col gap-1">
-			<Label
-				htmlFor="languages_spoken"
-				className={error ? 'text-destructive' : ''}
-			>
-				Do you know other languages?
-			</Label>
+			<div>
+				<Label
+					htmlFor="languages_spoken"
+					className={error ? 'text-destructive' : ''}
+				>
+					Do you know other languages?
+				</Label>
+				<p className="text-muted-foreground text-sm">
+					We'll show you translations available in any of the languages you're
+					comfortable with.
+				</p>
+			</div>
 			<FancyMultiSelect
 				options={allLanguageOptions}
 				selected={value}
 				setSelected={onChange}
 				always={primary ? languages[primary] : undefined}
+				placeholder="Select languages..."
 			/>
 			<ErrorLabel {...error} />
 		</div>
