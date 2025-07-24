1ec39781665e2b214317fee047267b9dd56b8175
Em Snook <me@msnook.xyz>
2025-07-24 23:27:33 +0530

    Fix this onClick for the share button

--- src/components/native-share-button.tsx
+++ src/components/native-share-button.tsx
@@ -1,6 +1,7 @@
 import toast from 'react-hot-toast'
 import { Share } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { useCallback } from 'react'
 
 export function NativeShareButton({
 	shareData,
@@ -8,19 +9,19 @@ export function NativeShareButton({
 	shareData: { text: string; title: string }
 }) {
 	const canShare = typeof navigator?.share === 'function'
-	if (!canShare) return null
 
-	const onClick = () => {
+	const onClick = useCallback(() => {
 		void navigator.share(shareData).catch((error: DOMException | TypeError) => {
-			console.log(
-				`Some error has occurred while sharing. It could just be they cancelled the share.`,
-				error
-			)
-			toast.error(
-				`Some error has occurred while trying to open your device's share screen 🙈 Sorry. Please try something else.`
-			)
+			if (error.name !== 'AbortError') {
+				console.log(`Some error has occurred while sharing.`, error)
+				toast.error(
+					`Some error has occurred while trying to open your device's share screen 🙈 Sorry. Please try something else.`
+				)
+			}
 		})
-	}
+	}, [shareData])
+
+	if (!canShare) return null
 
 	return (
 		<Button size="lg" onClick={onClick}>
