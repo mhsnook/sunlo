b3b0928656d189d0d3909ced94c127baa698cd14
Em <me@msnook.xyz>
2025-08-03 18:45:52 +0530

    Fix chats-sidebar overflow, fix height, loading skeleton

--- src/components/friends/chats-sidebar.tsx
+++ src/components/friends/chats-sidebar.tsx
@@ -1,7 +1,6 @@
 import { Link } from '@tanstack/react-router'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
-import { ScrollArea } from '../ui/scroll-area'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { useRelations } from '@/lib/friends'
 import { cn } from '@/lib/utils'
 
@@ -10,67 +9,77 @@ const linkActiveProps = {
 }
 
 export function ChatsSidebar() {
-	const { data: relations } = useRelations()
+	const { data: relations, isPending } = useRelations()
 
-	if (!relations) {
-		return <div>Loading friends...</div>
-	}
-
-	const friends = relations.uids.friends.map(
-		(uid) => relations.relationsMap[uid]
-	)
+	const friends = relations?.uids.friends
+		.map((uid) => relations.relationsMap[uid])
+		.filter((f) => f.status === 'friends')
 
 	// Mock sorting by recent activity
-	const sortedFriends = friends.sort((a, b) =>
+	const sortedFriends = friends?.sort((a, b) =>
 		a.most_recent_created_at == b.most_recent_created_at ? 0
 		: a.most_recent_created_at < b.most_recent_created_at ? 1
 		: -1
 	)
+
 	return (
-		<Card className="w-full">
+		<Card className="flex h-[calc(100vh-9rem-4px)] w-full flex-col">
 			<CardHeader className="sr-only">
 				<CardTitle>Chats</CardTitle>
 			</CardHeader>
-			<CardContent className="flex-1 p-0">
-				<ScrollArea className="">
-					<div className="flex flex-col gap-1 p-2">
-						{sortedFriends.length === 0 ?
-							<p className="text-muted-foreground p-4 text-sm">
-								You have no friends to chat with yet.
-							</p>
-						:	sortedFriends.map((friend) => (
-								<Link
-									key={friend.uidOther}
-									to="/friends/chats/$friendId"
-									params={{ friendId: friend.uidOther }}
-									className={cn(
-										'hover:bg-accent/30 hover:text-accent-foreground flex items-center gap-3 rounded-lg px-3 py-2 transition-all'
-									)}
-									activeProps={linkActiveProps}
-								>
-									<Avatar className="h-8 w-8">
-										<AvatarImage
-											src={friend.profile?.avatarUrl}
-											alt={friend.profile?.username}
-										/>
-										<AvatarFallback>
-											{friend.profile?.username?.charAt(0).toUpperCase()}
-										</AvatarFallback>
-									</Avatar>
-									<div className="flex-1 truncate">
-										<div className="font-semibold">
-											{friend.profile?.username}
-										</div>
-										<p className="text-muted-foreground truncate text-xs">
-											Mock message...
-										</p>
-									</div>
-								</Link>
-							))
-						}
-					</div>
-				</ScrollArea>
+			<CardContent className="flex flex-1 flex-col gap-2 overflow-y-auto p-2">
+				{isPending ?
+					<>
+						<ChatSkeleton />
+						<ChatSkeleton />
+						<ChatSkeleton />
+					</>
+				: !sortedFriends || !sortedFriends.length ?
+					<p className="text-muted-foreground p-4 text-sm">
+						You have no friends to chat with yet.
+					</p>
+				:	sortedFriends.map((friend) => (
+						<Link
+							key={friend.uidOther}
+							to="/friends/chats/$friendId"
+							params={{ friendId: friend.uidOther }}
+							className={cn(
+								'hover:bg-accent/30 hover:text-accent-foreground flex items-center gap-3 rounded-2xl px-3 py-2 transition-all'
+							)}
+							activeProps={linkActiveProps}
+						>
+							<Avatar className="h-8 w-8">
+								<AvatarImage
+									src={friend.profile?.avatarUrl}
+									alt={friend.profile?.username}
+								/>
+								<AvatarFallback>
+									{friend.profile?.username?.charAt(0).toUpperCase()}
+								</AvatarFallback>
+							</Avatar>
+							<div className="line-clamp-3 flex-1">
+								<div className="font-semibold">{friend.profile?.username}</div>
+								<p className="text-muted-foreground text-xs">
+									48m ago • You: mock message... mock message...
+								</p>
+							</div>
+						</Link>
+					))
+				}
 			</CardContent>
 		</Card>
 	)
 }
+
+const ChatSkeleton = () => (
+	<div className="flex h-20 animate-pulse flex-row items-center gap-3 rounded-2xl px-3 py-2">
+		<div className="h-8 w-8 rounded-full bg-gray-800/50"></div>
+		<div className="h-16 w-full flex-1 space-y-2 rounded">
+			<div className="flex flex-row gap-2">
+				<div className="h-4 w-[50%] rounded bg-gray-800/50"></div>
+				<div className="h-4 w-[10%] rounded bg-gray-800/50"></div>
+			</div>
+			<div className="h-8 w-[100%] rounded bg-gray-800/50"></div>
+		</div>
+	</div>
+)
