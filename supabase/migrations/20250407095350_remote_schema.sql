ab5a16da441cf3b3c3b04c74d891e030126d3105
Em Snook <me@msnook.xyz>
2025-08-27 20:55:05 -0400

    Remove unused permission changes in storage migration

--- supabase/migrations/20250407095350_remote_schema.sql
+++ supabase/migrations/20250407095350_remote_schema.sql
@@ -1,39 +1,3 @@
-grant delete on table "storage"."s3_multipart_uploads" to "postgres";
-
-grant insert on table "storage"."s3_multipart_uploads" to "postgres";
-
-grant references on table "storage"."s3_multipart_uploads" to "postgres";
-
-grant
-select
-	on table "storage"."s3_multipart_uploads" to "postgres";
-
-grant trigger on table "storage"."s3_multipart_uploads" to "postgres";
-
-grant
-truncate on table "storage"."s3_multipart_uploads" to "postgres";
-
-grant
-update on table "storage"."s3_multipart_uploads" to "postgres";
-
-grant delete on table "storage"."s3_multipart_uploads_parts" to "postgres";
-
-grant insert on table "storage"."s3_multipart_uploads_parts" to "postgres";
-
-grant references on table "storage"."s3_multipart_uploads_parts" to "postgres";
-
-grant
-select
-	on table "storage"."s3_multipart_uploads_parts" to "postgres";
-
-grant trigger on table "storage"."s3_multipart_uploads_parts" to "postgres";
-
-grant
-truncate on table "storage"."s3_multipart_uploads_parts" to "postgres";
-
-grant
-update on table "storage"."s3_multipart_uploads_parts" to "postgres";
-
 create policy "Allow users to select, insert, update 1oj01fe_0" on "storage"."objects" as permissive for insert to authenticated
 with
 	check ((bucket_id = 'avatars'::text));
