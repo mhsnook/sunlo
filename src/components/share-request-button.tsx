93d0f5499db10a7ccb9360c71847e309c502ee8c
Em Snook <me@msnook.xyz>
2025-09-18 15:49:18 +0530

    Add native share button to request-list-item

--- src/components/share-request-button.tsx
+++ src/components/share-request-button.tsx
@@ -48,7 +48,7 @@ export default function ShareRequestButton({
 			{...props}
 		>
 			<Share2 className="h-4 w-4" />
-			<span className="hidden @sm:block">{text}</span>
+			{size !== 'icon' && <span className="hidden @sm:block">{text}</span>}
 		</Button>
 	)
 }
