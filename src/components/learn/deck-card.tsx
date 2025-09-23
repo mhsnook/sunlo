60cac01d236e5b390cc9288e306fe194b263b661
Em Snook <me@msnook.xyz>
2025-09-24 05:07:24 +0530

    Make S and L composable too, for dark mode support

--- src/components/learn/deck-card.tsx
+++ src/components/learn/deck-card.tsx
@@ -13,92 +13,108 @@ import { Link } from '@tanstack/react-router'
 import { buttonVariants } from '../ui/button-variants'
 import { cn } from '@/lib/utils'
 import { DeckStatsBadges } from '../stats-badges'
+import { useEffect, useRef } from 'react'
+import { useProfile } from '@/hooks/use-profile'
 
 export function DeckCard({ deck }: { deck: DeckMeta }) {
-	return (
-		<Card className="@container relative overflow-hidden transition-all duration-200 hover:-translate-y-0.5">
-			<CardHeader className="from-primary/10 to-primary-foresoft/30 flex flex-row items-center justify-between gap-6 bg-gradient-to-br p-4 text-white">
-				<Link
-					className="grow"
-					to="/learn/$lang"
-					// oxlint-disable-next-line jsx-no-new-object-as-prop
-					params={{ lang: deck.lang! }}
-				>
-					<CardTitle className="text-primary-foresoft flex flex-row justify-between gap-2 text-xl">
-						<span>{deck.language}</span>
-					</CardTitle>
-				</Link>
-				<span className="flex items-center justify-between gap-2">
-					{deck.archived ?
-						<Badge variant="secondary">
-							<Archive />
-							Archived
-						</Badge>
-					:	<Link
-							to="/learn/$lang/review"
-							className={buttonVariants({ size: 'icon' })}
-							// oxlint-disable-next-line jsx-no-new-object-as-prop
-							params={{ lang: deck.lang! }}
-						>
-							<Rocket />
-						</Link>
-					}
-				</span>
-			</CardHeader>
+	const ref = useRef<HTMLDivElement | null>(null)
+	const { data: profile } = useProfile()
+	useEffect(() => {
+		const theme = profile?.decksMap?.[deck.lang!].theme
+		console.log(`running effect`, theme)
+		if (theme && ref.current) {
+			ref.current?.style.setProperty('--hue', String(theme.hue))
+			ref.current?.style.setProperty('--hue-off', String(theme.hueOff))
+			ref.current?.style.setProperty('--hue-accent', String(theme.hueAccent))
+		}
+	}, [profile, deck])
 
-			<CardContent className="space-y-2 p-4">
-				<div className="flex flex-wrap gap-2">
-					<DeckStatsBadges deckMeta={deck} />
-				</div>
-			</CardContent>
-			<CardFooter className="block w-full space-y-4 p-4 pt-0">
-				<div className="flex flex-row flex-wrap gap-2">
-					{deck.archived ?
-						<ArchiveDeckButton
-							className="w-full grow basis-60"
-							lang={deck.lang!}
-							archived={deck.archived}
-						/>
-					:	<Link
-							to="/learn/$lang"
-							className={cn(
-								buttonVariants({ variant: 'secondary' }),
-								'grow basis-40'
-							)}
-							// oxlint-disable-next-line jsx-no-new-object-as-prop
-							params={{ lang: deck.lang! }}
-						>
-							<HouseHeart />
-							Deck Home
-						</Link>
-					}
-					{!deck.archived ?
-						<Link
-							to="/learn/$lang/library"
-							className={cn(
-								buttonVariants({ variant: 'secondary' }),
-								`grow basis-60`
-							)}
-							// oxlint-disable-next-line jsx-no-new-object-as-prop
-							params={{ lang: deck.lang! }}
-						>
-							<BookOpenText />
-							Explore Library
-						</Link>
-					:	null}
-				</div>
-				{/* Subtle stats footer */}
-				<div className="text-muted-foreground border-t pt-2 text-xs">
-					{deck.cards_active! + deck.cards_learned!} total cards •{' '}
-					{!deck.cards_learned ?
-						`0 mastered`
-					:	`${(
-							(100 * deck.cards_learned) /
-							(deck.cards_learned + deck.cards_active!)
-						).toFixed(0)} % mastered`
-					}
-				</div>
-			</CardFooter>
-		</Card>
+	return (
+		<div ref={ref}>
+			<Card className="@container relative overflow-hidden transition-all duration-200 hover:-translate-y-0.5">
+				<CardHeader className="from-primary/10 to-primary-foresoft/30 flex flex-row items-center justify-between gap-6 bg-gradient-to-br p-4 text-white">
+					<Link
+						className="grow"
+						to="/learn/$lang"
+						// oxlint-disable-next-line jsx-no-new-object-as-prop
+						params={{ lang: deck.lang! }}
+					>
+						<CardTitle className="text-primary-foresoft flex flex-row justify-between gap-2 text-xl">
+							<span>{deck.language}</span>
+						</CardTitle>
+					</Link>
+					<span className="flex items-center justify-between gap-2">
+						{deck.archived ?
+							<Badge variant="secondary">
+								<Archive />
+								Archived
+							</Badge>
+						:	<Link
+								to="/learn/$lang/review"
+								className={buttonVariants({ size: 'icon' })}
+								// oxlint-disable-next-line jsx-no-new-object-as-prop
+								params={{ lang: deck.lang! }}
+							>
+								<Rocket />
+							</Link>
+						}
+					</span>
+				</CardHeader>
+
+				<CardContent className="space-y-2 p-4">
+					<div className="flex flex-wrap gap-2">
+						<DeckStatsBadges deckMeta={deck} />
+					</div>
+				</CardContent>
+				<CardFooter className="block w-full space-y-4 p-4 pt-0">
+					<div className="flex flex-row flex-wrap gap-2">
+						{deck.archived ?
+							<ArchiveDeckButton
+								className="w-full grow basis-60"
+								lang={deck.lang!}
+								archived={deck.archived}
+							/>
+						:	<Link
+								to="/learn/$lang"
+								className={cn(
+									buttonVariants({ variant: 'secondary' }),
+									'grow basis-40'
+								)}
+								// oxlint-disable-next-line jsx-no-new-object-as-prop
+								params={{ lang: deck.lang! }}
+							>
+								<HouseHeart />
+								Deck Home
+							</Link>
+						}
+						{!deck.archived ?
+							<Link
+								to="/learn/$lang/library"
+								className={cn(
+									buttonVariants({ variant: 'secondary' }),
+									`grow basis-60`
+								)}
+								// oxlint-disable-next-line jsx-no-new-object-as-prop
+								params={{ lang: deck.lang! }}
+							>
+								<BookOpenText />
+								Explore Library
+							</Link>
+						:	null}
+					</div>
+					{/* Subtle stats footer */}
+					<div className="text-muted-foreground border-t pt-2 text-xs">
+						{deck.cards_active! + deck.cards_learned!} total cards •{' '}
+						{!deck.cards_learned ?
+							`0 mastered`
+						:	`${(
+								(100 * deck.cards_learned) /
+								(deck.cards_learned + deck.cards_active!)
+							).toFixed(0)} % mastered`
+						}
+					</div>
+				</CardFooter>
+			</Card>
+		</div>
 	)
 }
