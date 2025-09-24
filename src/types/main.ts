dbe1e82fe6615ad57bce0c380f6942a1680f5f0d
Em <me@msnook.xyz>
2025-09-24 23:31:13 +0530

    Make computed property optional in type

--- src/types/main.ts
+++ src/types/main.ts
@@ -165,7 +165,7 @@ export type DeckRow = Tables<'user_deck'>
 export type DeckStub = Tables<'user_deck'>
 export type DeckInsert = TablesInsert<'user_deck'>
 export type DeckMeta = Tables<'user_deck_plus'> & {
-	cardsScheduledForToday: number
+	cardsScheduledForToday?: number
 	theme?: ThemeType
 	themeCss?: ThemeCSS
 }
