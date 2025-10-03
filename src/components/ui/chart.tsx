b5b5082c5175a12b3dc80c16390b5a77d18b84c4
Em Snook <me@msnook.xyz>
2025-10-03 23:45:01 +0530

    Remove redundant type coercion

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -145,7 +145,7 @@ const ChartTooltipContent = React.forwardRef<
 			const itemConfig = getPayloadConfigFromPayload(config, item, key)
 			const value =
 				!labelKey && typeof label === 'string' ?
-					config[label as keyof typeof config]?.label || label
+					config[label]?.label || label
 				:	itemConfig?.label
 
 			if (labelFormatter) {
@@ -353,9 +353,7 @@ function getPayloadConfigFromPayload(
 		] as string
 	}
 
-	return configLabelKey in config ?
-			config[configLabelKey]
-		:	config[key as keyof typeof config]
+	return configLabelKey in config ? config[configLabelKey] : config[key]
 }
 
 export {
