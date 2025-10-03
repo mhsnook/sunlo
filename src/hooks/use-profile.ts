bdfa2b0124ee9db64c81e0fe150b5f91de49dbb1
Em Snook <me@msnook.xyz>
2025-10-03 23:49:54 +0530

    Runtime change: sorting decks in profileQuery

--- src/hooks/use-profile.ts
+++ src/hooks/use-profile.ts
@@ -28,24 +28,32 @@ export const profileQuery = (userId: uuid | null) =>
 				.throwOnError()
 			if (data === null) return null
 			const { decks_array, ...profile } = data
-			const decksSorted = decks_array
-				.toSorted((a, b) =>
-					a.created_at === b.created_at ? 0
-					: a.created_at! > b.created_at! ? 1
-					: -1
-				)
-				.map((d, i) => {
-					const theme = themes[i % themes.length]
-					return {
-						...d,
-						theme,
-						themeCss: {
-							'--hue': theme?.hue,
-							'--hue-off': theme?.hueOff,
-							'--hue-accent': theme?.hueAccent,
-						} as CSSProperties,
-					}
-				})
+			const decksWithTheme = decks_array.map((d, i) => {
+				const theme = themes[i % themes.length]
+				return {
+					...d,
+					theme,
+					themeCss: {
+						'--hue': theme?.hue,
+						'--hue-off': theme?.hueOff,
+						'--hue-accent': theme?.hueAccent,
+					} as CSSProperties,
+				}
+			})
+			const decksSorted = decksWithTheme.toSorted((a, b) =>
+				(
+					(a.most_recent_review_at || a.created_at) ===
+					(b.most_recent_review_at || b.created_at)
+				) ?
+					0
+				: (
+					(a.most_recent_review_at || a.created_at!) >
+					(b.most_recent_review_at || b.created_at!)
+				) ?
+					-1
+				:	1
+			)
+
 			const decksMap: Omit<DecksMap, 'cardsScheduledForToday'> = mapArray<
 				Omit<DeckMeta, 'cardsScheduledForToday'>,
 				'lang'
