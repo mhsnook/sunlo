9d7cf2e1aac8566e53a0afadc44d11719a1c4f7f
Em <me@msnook.xyz>
2025-11-11 15:48:05 +0530

    Move AwaitingAuthLoader component to its own file

--- src/components/auth-context.tsx
+++ src/components/auth-context.tsx
@@ -11,7 +11,7 @@ import { useQueryClient } from '@tanstack/react-query'
 import type { RolesEnum, uuid } from '@/types/main'
 import supabase from '@/lib/supabase-client'
 import { myProfileCollection } from '@/lib/collections'
-import { Loader } from '@/components/ui/loader'
+import { AwaitingAuthLoader } from '@/components/awaiting-auth-loader'
 
 const emptyAuth = {
 	isLoaded: false,
@@ -112,46 +112,5 @@ export function AuthProvider({ children }: PropsWithChildren) {
 		<AuthContext.Provider value={value as AuthState}>
 			{value.isLoaded ? children : <AwaitingAuthLoader />}
 		</AuthContext.Provider>
-	)
-}
-
-function AwaitingAuthLoader() {
-	const [time, setTime] = useState(0)
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setTime((prev) => prev + 1)
-		}, 1000)
-		return () => clearInterval(interval)
-	}, [])
-
-	return (
-		<div className="flex h-full w-full flex-col items-center justify-center gap-4 py-10">
-			<p
-				className={`${time >= 5 ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300 ease-linear`}
-			>
-				Connecting for {time} seconds...
-			</p>
-
-			<Loader
-				size={40}
-				className={`${time >= 1 ? 'opacity-50' : 'opacity-0'} transition-opacity duration-300 ease-linear`}
-			/>
-
-			<div
-				className={`${time >= 10 ? 'opacity-100' : 'opacity-0'} max-w-120 space-y-4 text-center transition-opacity duration-300 ease-linear`}
-			>
-				<p>
-					FYI this should never take longer than about 1 second. It's possible
-					your internet connection is down.
-				</p>
-				<p>
-					If not that... consider{' '}
-					<a className="s-link" href="mailto:sunloapp@gmail.com">
-						contacting the team by email
-					</a>{' '}
-					or get in touch with Em directly (it may be affecting others too).
-				</p>
-			</div>
-		</div>
 	)
 }
