25abfad08af6a0d17dc080e09b2720ce8ddacacf
Em Snook <me@msnook.xyz>
2025-12-09 15:35:59 +0530

    Fix redirect in the .index page

--- src/routes/_user/learn/$lang.index.tsx
+++ src/routes/_user/learn/$lang.index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_user/learn/$lang/')({
-	loader: ({ params }) => {
-		void redirect({ to: '/learn/$lang/feed', params, replace: true })
+	beforeLoad: ({ params }) => {
+		return redirect({ to: '/learn/$lang/feed', params })
 	},
 })
