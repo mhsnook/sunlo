133a9677d92441f651bce1d387684ad3e87960b6
Em Snook <me@msnook.xyz>
2026-01-09 12:15:56 +0530

    ESLint config: Allow throwing redirect

--- src/routes/_user/learn/$lang.requests.index.tsx
+++ src/routes/_user/learn/$lang.requests.index.tsx
@@ -2,7 +2,6 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_user/learn/$lang/requests/')({
 	beforeLoad: ({ params }) => {
-		// eslint-disable-next-line @typescript-eslint/only-throw-error
 		throw redirect({ to: '/learn/$lang/feed', params, replace: true })
 	},
 })
